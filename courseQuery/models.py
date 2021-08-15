5aa8c960c286315e44731c13d9dff46706e808e8
Aiden <b10730032@gapps.ntust.edu.tw>
2021-08-16 04:18:54 +0800

    Feat: Establish the database
    Modify the models.py:
    - add the User class to storage the chat_id
    - add the Task class to storage the task
    
    Author: Aiden

--- courseQuery/models.py
+++ courseQuery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    chat_id = models.CharField(max_length=20, help_text="chat_id")
+
+
+class Task(models.Model):
+    courseCode = models.CharField(max_length=9, help_text="courseNum")
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
